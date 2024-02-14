2b1057cd883c35c77a61262b4bb43557c3993fc8
Chirag Juneja <chiragjuneja6@gmail.com>
2024-02-14 20:18:06 +0530

    setup update and gitignore update

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="imgcluster",
+    name="pinaka",
     version="0.0.1",
-    description="Unsupervised Image Clustering module",
-    package_dir={"": "imgcluster"},
-    packages=find_packages(where="imgcluster"),
-    author='Chirag Juneja',
-    author_email='chiragjuneja6@gmail.com',
+    description="Advanced Computer Vision Library",
+    package_dir={"": "pinaka"},
+    packages=find_packages(where="pinaka"),
+    author="Chirag Juneja",
+    author_email="chiragjuneja6@gmail.com",
 )

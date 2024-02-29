861fb4b9311fb25a78a8de3ee8681376e02f3761
Chirag Juneja <chiragjuneja6@gmail.com>
2024-02-29 19:43:38 +0530

    load weights from custom path for training added

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 import multiprocessing
 import torch
+import copy
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
@@ -21,21 +22,41 @@ def parse_args():
     parser.add_argument("--epochs", default=100, type=int)
     parser.add_argument("--device", default="cpu", type=str)
     parser.add_argument("--imgsz", default=32, type=int)
-    parser.add_argument("-ckpt", "--checkpoints", default="./checkpoints", type=str)
+    parser.add_argument("--ckpt", "--checkpoints", default="./checkpoints", type=str)
     parser.add_argument("--data", default="./dataset", type=str)
+    parser.add_argument("--weights", default=None, type=str)
     return parser.parse_args()
 
 
-def train(epochs, batch_size, device, checkpoints, data, imgsz):
+def train(
+    epochs=100,
+    batch_size=64,
+    device="cuda",
+    data="./dataset",
+    imgsz=96,
+    checkpoints="./checkpoints/",
+    weights=None,
+    **kwargs,
+):
     transform = transforms.Compose(
         [transforms.Resize((imgsz, imgsz)), transforms.ToTensor()]
     )
 
     model = AutoEncoder()
+    if weights:
+        try:
+            model.load_state_dict(torch.load(weights))
+            logging.info(f"Model weights restored from {weights}")
+        except Exception as e:
+            logging.warning(f"Model load failed. {weights}\n {e}")
 
     dataset = ImageDataset(data, transforms=transform)
     dataloader = DataLoader(
-        dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=NUM_WORKER
+        dataset=dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=NUM_WORKER,
+        pin_memory=True,
     )
 
     mse = nn.MSELoss()
@@ -43,6 +64,8 @@ def train(epochs, batch_size, device, checkpoints, data, imgsz):
 
     model.to(device)
 
+    best_weights = copy.deepcopy(model.state_dict())
+    best_loss = float("inf")
     for epoch in range(epochs):
         for x in tqdm(dataloader):
             x = x.to(device)
@@ -51,20 +74,24 @@ def train(epochs, batch_size, device, checkpoints, data, imgsz):
             loss = mse(output, x)
             loss.backward()
             optimizer.step()
+        if loss < best_loss:
+            best_loss = loss
+            best_weights = copy.deepcopy(model.state_dict())
+
         logging.info(f"Epoch: [{epoch+1}/{epochs}], Loss: {loss.mean()}")
         if epoch % 10 == 0 and epoch:
             torch.save(model.state_dict(), checkpoints + f"/checkpoint-{epoch}.pt")
+    model.load_state_dict(best_weights)
+    torch.save(model.state_dict(), checkpoints + "/best.pt")
 
-    torch.save(model.state_dict(), checkpoints + "/model.pt")
 
-
-def make_dirs(args):
-    Path(args["checkpoints"]).mkdir(parents=True, exist_ok=True)
+def make_dirs(checkpoints="./checkpoints/", **kwargs):
+    Path(checkpoints).mkdir(parents=True, exist_ok=True)
 
 
 if __name__ == "__main__":
     args = parse_args()
     args = vars(args)
     logging.info(args)
-    make_dirs(args)
+    make_dirs(**args)
     train(**args)
